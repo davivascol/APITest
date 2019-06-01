11c699a003c9657f43f93c4d336ddacc41cc2ef3
Carlos Fonseca <davivascol@gmail.com>
2019-06-01 14:17:30 -0300

    Api teste finalizado

--- src/components/NewPost/NewPost.js
+++ src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -9,6 +10,18 @@ class NewPost extends Component {
         author: 'Max'
     }
 
+    postDataHandler = () => {
+      const dataPost = {
+        title: this.state.title,
+        body: this.state.content,
+        author: this.state.author
+      }
+      axios.post('/posts/', dataPost)
+        .then(response => {
+          console.log(response);
+        });
+    }
+    
     render () {
         return (
             <div className="NewPost">
@@ -22,7 +35,7 @@ class NewPost extends Component {
                     <option value="Max">Max</option>
                     <option value="Manu">Manu</option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
